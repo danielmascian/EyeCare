ee3860d73f5d9545581ff03bb10a05c7f5c50266
Daniel <danielmascian@gmail.com>
2026-06-28 19:11:02 +0300

    feat: Add simple timer with start and stop function

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-def main():
-    pass
+from tkinter import *
+from tkinter import ttk
+from timer import Timer
+
+root = Tk()
+root.title("Eyecare")
+
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+
+timer_label = ttk.Label(frm, text="Minutes: 0, Seconds: 0")
+timer_label.grid(column=0, row=1)
+
+timer = Timer(root, timer_label)
+
+ttk.Label(frm, text="Eyecare").grid(column=0, row=0)
+
+ttk.Button(frm,text="Timer Start",command=lambda: timer.start(100)).grid(column=1, row=0)
+
+ttk.Button(frm,text="Timer Stop",command=timer.stop).grid(column=2, row=0)
+
+ttk.Button(frm,text="Quit",command=root.destroy).grid(column=3, row=0)
+
+root.mainloop()
---
+++ timer.py
@@ -0,0 +1,31 @@
+class Timer:
+    def __init__(self, root, label):
+        self.root = root
+        self.label = label
+        self.time_left = 0
+        self.timer_id = None
+
+    def start(self, seconds):
+        self.stop()
+
+        self.time_left = int(seconds)
+        self.update_label()
+        self.tick()
+
+    def tick(self):
+        if self.time_left > 0:
+            self.time_left -= 1
+            self.update_label()
+            self.timer_id = self.root.after(1000, self.tick)
+        else:
+            self.label.config(text="Time is over!")
+            self.timer_id = None
+
+    def stop(self):
+        if self.timer_id is not None:
+            self.root.after_cancel(self.timer_id)
+            self.timer_id = None
+
+    def update_label(self):
+        minutes, seconds = divmod(self.time_left, 60)
+        self.label.config(text=f"Minutes: {minutes}, Seconds: {seconds}")
